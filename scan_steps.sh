4af9e9182b8237f0009c84ba5f67e9b11bcc6a27
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-05-10 15:25:55 +0530

    Create scan_steps.sh

---
+++ scan_steps.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo docker pull golang
+sudo docker run golang
