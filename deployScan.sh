1c45feb54c0d984b33471642dce227dbb82db8e5
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-09-12 13:21:45 +0530

    Update deployScan.sh

--- deployScan.sh
+++ deployScan.sh
@@ -1,13 +1,3 @@
-sudo apt update
-# install docker engine and start
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo apt update && sudo apt install docker.io -y
 # pull and run an image 
 sudo docker pull golang
-sudo docker run golang
